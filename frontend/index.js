3676f3cab320aa1777a8d6cd0524a5ae39c5509b
ktan088 <ktan0088@student.monash.edu>
2023-07-18 16:52:20 +0800

    changed frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
-fetch('https://test-node-vercel-roan.vercel.app/')
-  .then(data => {
-    console.log(data); // Do something with the response data
-  })
-  .catch(error => {
-    console.error('Error:', error); // Handle any errors that occur during the request
-  });
+// fetch('https://test-node-vercel-roan.vercel.app/')
+//   .then(data => {
+//     console.log(data); // Do something with the response data
+//   })
+//   .catch(error => {
+//     console.error('Error:', error); // Handle any errors that occur during the request
+//   });
+
+document.write("<h1> hello from frontend </h1>")
