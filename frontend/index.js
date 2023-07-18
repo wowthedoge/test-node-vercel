506fabd8e1283a8ca56ceed3ba92146005185a83
ktan088 <ktan0088@student.monash.edu>
2023-07-18 16:34:25 +0800

    added backend, fetch in frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,7 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
-
-app.use(express.json()); // Middleware to parse JSON request bodies
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳');
-});
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ');
-});
-
-app.post('/', (req, res) => {
-  res.send('Just posted a request');
-});
-
-// Export the Express API
-module.exports = app;
+fetch('https://test-node-vercel-roan.vercel.app/')
+  .then(data => {
+    console.log(data); // Do something with the response data
+  })
+  .catch(error => {
+    console.error('Error:', error); // Handle any errors that occur during the request
+  });
